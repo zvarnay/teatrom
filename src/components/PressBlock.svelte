48bcb6039eb326e8da4bcd08e0b2811a0fb830f1
Mihály Hideg <hidegmisi@gmail.com>
2024-09-28 00:18:33 +0200

    press page partially done

---
+++ src/components/PressBlock.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+    export let title;
+    export let body = "";
+    export let links: { title: string; year: string; url: string; image: string }[] = [];
+</script>
+
+<section class="pressBlock">
+    <div class="contentWrapper">
+        <h2>{title}</h2>
+        {#if !!body}
+            <p>{@html body}</p>
+        {/if}
+        <div class="grid">
+            {#each links as link}
+                <article>
+                    <a href={link.url} target="_blank">
+                        <img src={link.image} alt="">
+                        <h3>{link.title}</h3>
+                        <p>{link.year}</p>
+                    </a>
+                </article>
+            {/each}
+        </div>
+    </div>
+</section>
+
+<style lang="scss">
+    section {
+        display: flex;
+        align-items: center;
+        margin: 0 -1rem;
+        padding: 3rem 2rem;
+        background-image: linear-gradient(90deg, #f5f1cd, #eda87b);
+        background-size: cover;
+        
+        position: relative;
+        z-index: 2;
+
+        .grid {
+            display: grid;
+            grid-template-columns: 1fr;
+            gap: 1rem;
+            margin: 0 auto;
+
+            article {
+                display: flex;
+                flex-direction: column;
+                gap: 1rem;
+                padding: 1rem;
+                background: var(--color-theme-2);
+                box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
+
+                img {
+                    margin-top: 1rem;
+                    width: 100%;
+                }
+
+                h3 {
+                    margin-top: 1rem;
+                    font-size: var(--font-size-subheader);
+                    font-weight: 600;
+                }
+
+                p {
+                    margin-top: 0rem;
+                    font-size: var(--font-size-caption);
+                    color: var(--color-text-light);
+                }
+            }
+        }
+    }
+
+    .contentWrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        margin: 0 auto;
+        max-width: 52rem;
+        width: 100%;
+    }
+
+    h2 {
+        font-size: var(--font-size-header);
+        font-weight: 600;
+    }
+
+    section :global(p) {
+        margin-top: 1.5rem;
+        font-size: var(--font-size-body);
+        line-height: 1.5;
+    }
+
+    @media (min-width: 680px) {
+        section {
+            min-height: 90vh;
+
+            .grid {
+                grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+                gap: 2rem;
+            }
+        }
+    }
+</style>
--- src/routes/press/+page.svelte
+++ src/routes/press/+page.svelte
@@ -2,15 +2,34 @@
     import ContentBlock from "../../components/ContentBlock.svelte";
     import DonateBlock from "../../components/DonateBlock.svelte";
     import HeaderBlock from "../../components/HeaderBlock.svelte";
+    import PressBlock from "../../components/PressBlock.svelte";
 </script>
 
 <section>
     <HeaderBlock title="Sajtómegjelenések" />
-    <ContentBlock 
+    <!-- <ContentBlock 
         title="Sajtómegjelenések"
         body="<p>A belépés in­gye­nes. Fenn­tar­tási költ­sé­ge­ink fe­de­zé­sére azonban meg­ké­rünk minden olyan lá­to­ga­tót, akinek le­he­tő­sé­gé­ben áll, hogy tá­mo­gas­son minket aján­lott összegű ado­mánnyal (3500 Ft egy nap, 6000 Ft két nap). Fi­ze­tésre a hely­szí­nen, kész­pénz­zel lesz le­he­tő­ség!</p>"
         background="url(/images/textures/tx-1.webp)"
         mini={true}
+    /> -->
+    <PressBlock
+        title="Rádióinterjúk"
+        links="{[
+            {
+                title: 'Ötös – Beszélgetés Simon Balázzsal',
+                year: '2024',
+                url: 'https://www.klubradio.hu/archivum/otos-muranyi-tundevel-es-kalid-arturral-2024-januar-19-pentek-1005-38450',
+                image: 'https://ipi.media/wp-content/uploads/2021/06/IPI-Hungary-Klubradio-EU-Statement-09-06-2021.jpg'
+            },
+            {
+                title: 'Bari Judit és Éva Mélyinterjúja Simon Balázzsal',
+                year: '2023',
+                url: 'https://tilos.hu/episode/cekla/2023/11/05',
+                image: 'https://ipi.media/wp-content/uploads/2022/05/IPI-Hungary-Tilos-Radio-MFRR-statement-12-05-2022-1.jpg'
+            },
+        ]}"
     />
     <DonateBlock />
 </section>
+
