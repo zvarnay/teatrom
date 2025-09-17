78be02de779566805ba5cec420edb05df3276ee1
zvarnay <zvarnay@gmail.com>
2025-09-17 13:22:34 +0200

    2025 hozzáadás

--- src/routes/[lang]/archive/[year]/+page.server.ts
+++ src/routes/[lang]/archive/[year]/+page.server.ts
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
-	if ( ['2020', '2021', '2022', '2023', '2024'].find(v => v === params.year) ) {
+	if ( ['2020', '2021', '2022', '2023', '2024', '2025'].find(v => v === params.year) ) {
         return {
             year: Number(params.year),
         };
